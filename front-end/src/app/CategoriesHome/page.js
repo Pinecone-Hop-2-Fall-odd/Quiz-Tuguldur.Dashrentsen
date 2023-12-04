558c106baa5bfc5ca8213ced2bf1a452d2c6c1da
Tuguldur Dashrentsen <HP230089@G303-CC0131.local>
2023-12-04 19:22:19 +0800

    quiz and category

---
+++ front-end/src/app/CategoriesHome/page.js
@@ -0,0 +1,14 @@
+export default function Categories() {
+  return (
+    <div className="flex flex-col justify-center items-center w-screen h-screen ">
+      <div className="w-[600px] h-[800px] border-solid border-[2px] flex flex-col items-center ">
+        <div className="w-[600px] h-[90px] bg-[#1A8BBB] flex flex-col items-center justify-center text-[20px] text-white ">
+          <h1 className=" ">What type of quiz do you want to play?</h1>
+        </div>
+        <div className="px-[20px] w-[600px] h-[710px] bg-red-100">
+            
+        </div>
+      </div>
+    </div>
+  );
+}
--- front-end/src/app/QuizPlayHome/page.js
+++ front-end/src/app/QuizPlayHome/page.js
@@ -3,17 +3,23 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { Montserrat } from "next/font/google";
 
 export default function HomePage() {
   const router = useRouter();
-  const [data, setData] = useState();
+  const [quizdata, setQuizData] = useState();
   const [counter, setCounter] = useState(0);
   const [iscorrect, setIscorrect] = useState(null);
+  const [isCorrectCounter, setIsCorrectCounter] = useState(0);
+  const [userData, setUserData] = useState();
 
   const getData = async () => {
-    const data = await axios.get("http://localhost:8000/allQuiz");
-    console.log(data);
-    setData(data);
+    const userId = localStorage.getItem("uid");
+    const quizData = await axios.get("http://localhost:8000/allQuiz");
+    const userData = await axios.get(`http://localhost:8000/user/${userId}`);
+    setQuizData(quizData);
+    setUserData(userData);
+    console.log(userData);
   };
 
   useEffect(() => {
@@ -22,10 +28,11 @@ export default function HomePage() {
 
   function isCorrect(answer, index) {
     if (answer.isCorrect == true) {
-      console.log("correct");
       setIscorrect(index);
+      alert("Correct");
+      setIsCorrectCounter(isCorrectCounter + 1);
     } else {
-      console.log("incorrect");
+      alert("Incorrect");
     }
     console.log(answer);
 
@@ -33,22 +40,55 @@ export default function HomePage() {
 
     console.log(counter);
   }
+  function homePush(){
+    router.push("/HomePageHome")
+  }
+
+  if (quizdata?.data?.quizs.length === counter) {
+    return (
+      <div class="justify-center items-center  relative bg-gray-400 w-screen h-screen flex-col flex gap-[40px]">
+        <div className="absolute top-[0px] w-screen h-[135px] bg-white flex items-center ">
+          <h1 className="absolute left-[890px] font-bold text-[50px] text-[#50566B] font-montserrat">
+            "Congratulations"
+          </h1>
+        </div>
+        <div className="relative flex flex-col items-center pt-[40px] bg-white w-[700px] h-[860px] rounded-[30px]">
+          <h1 className="font-bold text-[50px] text-[#50566B] flex flex-col items-center">
+            {userData?.data?.user[0]?.userName}
+          </h1>
+          <h1 className="font-bold text-[50px] text-[#50566B]">
+            You got {isCorrectCounter} out of {quizdata?.data?.quizs.length}{" "}
+            right
+          </h1>
+          <div className="right-[100px] flex flex-row gap-[30px] absolute bottom-[100px] ">
+            <button className="text-white text-[20px] bg-[#1A8BBB] w-[100px] h-[60px] rounded-[10px] ">Categories</button>
+            <button onClick={homePush} className="text-white text-[20px] bg-[#1A8BBB] w-[100px] h-[60px] rounded-[10px]">Home</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
-    <div class="w-screen h-screen flex-col flex gap-[40px] justify-center">
-      <h1 className=" text-[80px] text-[#50566B] font-semibold">
-        Question:{data?.data?.quizs[counter]?.question}
-      </h1>
-      <div className="flex gap-[30px] flex-col">
-        {data?.data?.quizs[counter]?.answers.map((answer, index) => (
+    <div class=" relative bg-gray-400 w-screen h-screen flex-col flex gap-[40px]">
+      <div className="absolute top-[0px] w-screen h-[135px] bg-white flex items-center ">
+        <h1 className="absolute left-[890px] font-bold text-[50px] text-[#50566B] font-montserrat">
+          {quizdata?.data?.quizs[counter]?.question}
+        </h1>
+        <h1 className="absolute right-[100px] font-bold text-[50px] text-[#50566B] font-montserrat">
+          {quizdata?.data?.quizs.length}/{counter}
+        </h1>
+      </div>
+      <div className="g-[30px] absolute left-[80px] bottom-[50px] w-[2900px] flex flex-row gap-[30px] flex-wrap">
+        {quizdata?.data?.quizs[counter]?.answers.map((answer, index) => (
           <button
-            style={
-              iscorrect === index
-                ? { background: "green" }
-                : { background: "white" }
-            }
+            // style={
+            //   iscorrect === index
+            //     ? { background: "green" }
+            //     : { background: "white" }
+            // }
             onClick={() => isCorrect(answer, index)}
-            className="bg-white border-solid border-[1px] border-black"
+            className="pl-[10px] flex justify-start items-center text-[50px] w-[1000px] h-[250px]  bg-white border-solid border-[1px] border-black"
           >
             {answer?.answer}
           </button>
